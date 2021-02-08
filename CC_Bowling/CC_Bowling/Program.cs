69f18ed95bccf790f6acd9fe6b7e58e055a1d1ed
tuda224 <johann.tuder@gmail.com>
2021-02-08 19:40:34 +0100

    initial project

---
+++ CC_Bowling/CC_Bowling/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CC_Bowling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
